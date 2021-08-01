3b825d12aa217f45f491c1238d1984c8591b2263
saqebkhan <saqebk619@gmail.com>
2021-08-02 00:53:38 +0530

    Create evenNumberFinder.js

---
+++ evenNumberFinder.js
@@ -0,0 +1,28 @@
+// let evenNumber=(num)=>{
+//     if(num%2==0){
+//         console.log("even hai")
+//     }
+//     else{
+//         console.log("odd Hai")
+//     }
+
+// }
+// evenNumber(3)
+
+// let evennumber =(a) =>(a%2===0)
+// console.log(evennumber(20))
+
+var powerFinder=(akda,upperka)=>{
+    jawab=akda**upperka
+    console.log(jawab)
+
+}
+powerFinder(2,3)
+
+
+function raiseto(number,raise){
+    utter=number**raise
+    console.log(utter)
+
+}
+raiseto(3,2)
