4c34727a893f6ecc686f857bbe03660edcb9f822
saqebkhan <saqebk619@gmail.com>
2021-08-02 00:53:16 +0530

    Update BiggerNum.js

--- BiggerNum.js
+++ BiggerNum.js
@@ -1,7 +1,8 @@
-function BiggerNum(num1, num2){
+let BiggerNum=(num1, num2)=>{
     if(num1>num2){
         return num1;
-    }else{
+    }
+    else{
         return num2;
     }
 }
@@ -12,6 +13,10 @@ console.log(`Bigger Number is ${BiggerNum(-100000000,1)}`);
 console.log(`Bigger Number is ${BiggerNum(7849,498)}`);
 console.log(`Bigger Number is ${BiggerNum(5886,100)}`);
 
+// let bada=(num1,num2)=>{num= num1>num2 ? num1 : num2
+//     console.log(num)
+// }
+// bada(11,2)
 
 
 
