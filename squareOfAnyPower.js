d36dd26a352a3a1468a4f0f75287e7afe5e3db48
saqebkhan <saqebk619@gmail.com>
2021-08-03 00:21:22 +0530

    Update squareOfAnyPower.js

--- squareOfAnyPower.js
+++ squareOfAnyPower.js
@@ -5,6 +5,23 @@
 // console.log(SquareOfAnyPower([4,5,6,7,8,9,], 2));
 
 
+const obj={
+    
+    Data:[
+        {
+            name:"saqeb",
+            fathername:"samad",
+            surname:"khan"
+        },
+        {
+            name:"zaid",
+            father:"zabi",
+            surname:"mirza"
+        }
+    ]
+    
 
+}
+console.log(obj.includes('me'))
 
 
