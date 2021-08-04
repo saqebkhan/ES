d4bec40f64bc423bae9bd1651c23fbfd549d8e46
saqebkhan <saqebk619@gmail.com>
2021-08-04 16:16:41 +0530

    Create exam.js

---
+++ exam.js
@@ -0,0 +1,44 @@
+// var a=("Saqeb")
+// console.log(a)
+
+// var b = 9
+// console.log(b)
+
+// var c = true
+// console.log(c)
+
+// var d = null
+// console.log(d)
+
+// var e = ["students", "employers"]
+// console.log(e)
+
+// var f ={
+//     name:"saqeb",
+//     surname:"khan"
+// }
+// var g
+
+// console.log(g)
+
+
+// var solutions=(x,y)=>{
+//     var addition=x+y
+//     var substraction=x-y
+//     var multiplication=x*y
+//     var division=x/y
+//     var modulus=x%y
+
+//     console.log(`addition = ${addition}, substraction = ${substraction} multiplication = ${multiplication} division = ${division} modulus = ${modulus}`)
+
+// }
+// solutions(3,2)
+
+
+
+const express =require("express")
+const app = express();
+
+if(app.listen(4000)){
+    console.log("connected to local server:4000")
+}
