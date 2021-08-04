84231a6275827d9c9233fc32f9dc9f655b05c062
saqebkhan <saqebk619@gmail.com>
2021-08-04 16:16:53 +0530

    Create app.js

---
+++ MyAppsServer/app.js
@@ -0,0 +1,15 @@
+
+// const express =require("express")
+// const app = express();
+
+// if(app.listen(4786)){
+//     console.log("connected to local server:4786")
+// }
+
+const express =require("express")
+const app = express();
+
+if(app.listen(4666)){
+    console.log("connected to local server:4666")
+}
+
