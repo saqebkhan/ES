97f9e504cff212440e296d99a4210bd296695356
saqebkhan <saqebk619@gmail.com>
2021-08-12 15:22:15 +0530

    Create subsExitsts.js

---
+++ subsExitsts.js
@@ -0,0 +1,46 @@
+// const subsExists =(str)=>{
+// const arrStr=str.split(" ")
+// let cnt = 0;
+// arrStr.forEach((element) => {
+//     if (element.toUpperCase() === "part".toUpperCase()){
+//         cnt = cnt+ 1
+//     }
+    
+// });
+// return cnt;
+// }
+// const strData ="the main written part of a book, newspaper, etc"
+
+// console.log(subsExists(strData))
+
+
+// const subsExists = (str)=>{
+//     return str.split(" ").filter((item)=> item === "part").length;
+// }
+// const strData = "im the part of your life so this as a part good part"
+// console.log(subsExists(strData))
+
+
+
+
+// const subsExists = (str)=>{
+//     return str.split(" ").includes("nice")
+// }
+// const strData = "im the part of your life so this as a part good part"
+// console.log(subsExists(strData))
+
+
+
+// const subExists=(str)=>{
+//     arrStr=str.split(" ")
+//     cnt =0
+//     for (i=0; i<arrStr.length==="lorem"; i++){
+//         cnt = cnt + 1
+
+
+//     }
+
+
+
+// }
+
